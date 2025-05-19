6fa316ebc4ef22b368e53a9340a693f6fa676150
yang-pc2025 <yang.peiching@mail.cjcu.edu.tw>
2025-05-19 11:46:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 My new app 0519")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
